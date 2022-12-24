9adc490f4aa261c4fd64e04c837398e29137cb23
Cedar Chavez <geggas00@gmail.com>
2022-12-23 19:20:47 -0800

    add readme

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -36,10 +36,7 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">
-            🛍️
-          </span>
-          -Shop-Shop
+          E-Commerce
         </Link>
       </h1>
 
